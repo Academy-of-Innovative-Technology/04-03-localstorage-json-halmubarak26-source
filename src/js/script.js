7cf68049cfc814c67ba9b5209c9b6db2fdcceb94
halmubarak26-source <halmubarak26@aoiths.org>
2026-03-11 12:36:43 -0400

    javascript
    Load data from localStorage and display it in the DOM.

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,17 @@
+
+
+
 var Database = {
 	keyName: "Mutant Database",
 	data: []
+	
 };
 
 
 function loadDataSource() {
 
+	let storedData = localStorage.getItem(Database.keyName);
+	let setData = JSON.parse(storedData);
 	
 	displayData( Database.data );
 }
@@ -13,8 +19,14 @@ function loadDataSource() {
 
 
 function displayData(dataArray) {
+	let container = document.querySelector("#mutants");
+	dataArray.forEach(function(mutant){
+		let html = "<div> <h2>${mutant.name}</h2> <h3>${mutant.power}<h3> </div> ";
+	
+	});
+	
 
-
+	container.insertAdjacentHTML("beforeend",html);
 }
 
 loadDataSource();
